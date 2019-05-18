9492ef1ee3dabcbd985238fad51b94450e8a7553
David Kamer <david.kamer@uniwrighte.com>
2019-05-18 12:10:21 -0400

    meoved default urls to install and reloading when reset

--- background.js
+++ background.js
@@ -1,16 +1,18 @@
 // const DEFAULT_URLS = ['facebook.com', 'youtube.com', 'reddit.com']
-import { test } from './test.js'
 function getTimestamp (date = new Date()) {
   return new Date(date).getTime() / 1000 | 0
 }
 function listener () {
-  chrome.storage.sync.get(['excuses', 'debounce'], function (result) {
+  console.log('triggered')
+  chrome.storage.sync.get(['excuses', 'debounce', 'urls'], function (result) {
     const prevExcuses = result.excuses ? result.excuses : [] // TODO - move to storage.onChanged
-    const { debounce } = result
+    const { debounce, urls } = result
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
       const { url } = tabs[0]
+      const isCorrectUrl = urls.findIndex(u => url.indexOf(u) !== -1) !== -1
+      console.log({ isCorrectUrl })
       const debounceUrl = Object.keys(debounce).indexOf(url) !== -1 ? debounce[url] : null
-      if (!debounceUrl || getTimestamp(debounceUrl) + 10 < getTimestamp()) {
+      if ((!debounceUrl || getTimestamp(debounceUrl) + 10 < getTimestamp()) && isCorrectUrl) {
         const choice = window.prompt('Why did you decide to go to this site?', '')
         const date = new Date().toString()
         const excuses = [ ...prevExcuses, { date, choice, url } ]
@@ -21,39 +23,45 @@ function listener () {
 }
 
 function setupUrlListener () {
+  chrome.storage.sync.get('urls', function (urlObj) {
+    const { urls } = urlObj
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+      urls.forEach(url => {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: url }
+          })
+          ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+        }])
+      })
+      chrome.webNavigation.onDOMContentLoaded.removeListener(listener)
+      chrome.webNavigation.onDOMContentLoaded.addListener(listener, { url: urls.map(urlContains => ({ urlContains })) })
+    })
+  })
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+  console.log('the chrome extension is installing')
   chrome.runtime.getPackageDirectoryEntry(function (root) {
     root.getFile('dat/defaultUrls.json', {}, function (fileEntry) {
       fileEntry.file(function (file) {
         var reader = new FileReader()
         reader.onloadend = function (e) {
           const DEFAULT_URLS = JSON.parse(this.result)
-          chrome.storage.sync.get('urls', function (urlObj) {
-            const urls = urlObj.urls && urlObj.urls.length !== 0 ? urlObj.urls : DEFAULT_URLS
-            chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-              urls.forEach(url => {
-                chrome.declarativeContent.onPageChanged.addRules([{
-                  conditions: [new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: { urlContains: url }
-                  })
-                  ],
-                  actions: [new chrome.declarativeContent.ShowPageAction()]
-                }])
-              })
-              chrome.webNavigation.onDOMContentLoaded.removeListener(listener)
-              chrome.webNavigation.onDOMContentLoaded.addListener(listener, { url: urls.map(urlContains => ({ urlContains })) })
-            })
+          chrome.storage.sync.get('urls', function (result) {
+            const { urls } = result
+            if (!urls || urls.length === 0) chrome.storage.sync.set({ urls: DEFAULT_URLS })
+            setupUrlListener()
           })
         }
         reader.readAsText(file)
       })
     })
   })
-}
-
-chrome.runtime.onInstalled.addListener(function () {
-  test()
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-  })
+})
+chrome.runtime.onStartup.addListener(function () {
+  console.log('the chrome extension is starting up')
   setupUrlListener()
 })
 
