ddc5554da521cf8b6a287a678b4752597dab7463
David Kamer <david.kamer@uniwrighte.com>
2019-05-18 12:11:05 -0400

    removed read of default urls - done at install now

--- options.js
+++ options.js
@@ -33,77 +33,67 @@ function createLi (url, urls) {
 
 const page = document.getElementById('optionsDiv')
 function createOptions () {
-  chrome.runtime.getPackageDirectoryEntry(function (root) {
-    root.getFile('dat/defaultUrls.json', {}, function (fileEntry) {
-      fileEntry.file(function (file) {
-        var reader = new FileReader()
-        reader.onloadend = function (e) {
-          const DEFAULT_URLS = JSON.parse(this.result)
-          /* do here whatever with your JS object(s) */
-          const resetBtn = document.createElement('button')
-          resetBtn.addEventListener('click', function () {
-            chrome.storage.sync.set({ excuses: [], urls: [], debounce: {} }, function () {
-              document.getElementById('msg').innerHTML = 'you have reset all data!'
-            })
-          })
-          const eraseHistory = document.createElement('button')
-          eraseHistory.addEventListener('click', () => {
-            downloadExcuses()
-            chrome.storage.sync.set({ excuses: [] }, function () {
-              console.log('excuses downloaded and reset')
-            })
-          })
-          const addForm = document.createElement('form')
-          const urlInput = document.createElement('input')
-          const addBtn = document.createElement('button')
-          addForm.addEventListener('submit', e => {
-            e.preventDefault()
-          })
-          addForm.id = nameAsString({ addForm })
-          urlInput.id = 'urlInput'
-          addBtn.id = nameAsString({ addBtn })
-          resetBtn.innerText = 'reset'
-          eraseHistory.innerText = 'Download and Erase History'
-          addBtn.innerText = 'add url'
-          addBtn.addEventListener('click', () => {
-            const regex = /[-a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
-            const url = document.getElementById('urlInput')
-            const { value } = url
-            const isUrl = regex.test(value)
-            if (isUrl) {
-              // add url to list
-              chrome.storage.sync.get('urls', function (result) {
-                const urls = result.urls && result.urls.length > 0 ? result.urls : DEFAULT_URLS
-                const urlsUpdated = [...urls, value]
-                const ul = document.getElementById('ul')
-                const urlLi = createLi(value, urls)
-                ul.appendChild(urlLi)
-                chrome.storage.sync.set({ urls: urlsUpdated })
-              })
-            } else {
-              document.getElementById('msg').innerHTML = `<div class="error">invalid url</div>`
-            }
-          })
-          addForm.appendChild(urlInput)
-          addForm.appendChild(addBtn)
-          page.appendChild(addForm)
-          page.appendChild(eraseHistory)
-          chrome.storage.sync.get('urls', function (result) {
-            const urls = result.urls && result.urls.length > 0 ? result.urls : DEFAULT_URLS
-            const ul = document.createElement('ul')
-            ul.id = nameAsString({ ul })
-            urls.forEach(url => {
-              const urlLi = createLi(url, urls)
-              ul.appendChild(urlLi)
-            })
-            page.appendChild(ul)
-          })
-          page.appendChild(resetBtn)
-        }
-        reader.readAsText(file)
-      })
+  const resetBtn = document.createElement('button')
+  resetBtn.addEventListener('click', function () {
+    chrome.storage.sync.set({ excuses: [], urls: [], debounce: {} }, function () {
+      document.getElementById('msg').innerHTML = 'you have reset all data!'
+      chrome.runtime.reload()
     })
   })
+  const eraseHistory = document.createElement('button')
+  eraseHistory.addEventListener('click', () => {
+    downloadExcuses()
+    chrome.storage.sync.set({ excuses: [] }, function () {
+      console.log('excuses downloaded and reset')
+    })
+  })
+  const addForm = document.createElement('form')
+  const urlInput = document.createElement('input')
+  const addBtn = document.createElement('button')
+  addForm.addEventListener('submit', e => {
+    e.preventDefault()
+  })
+  addForm.id = nameAsString({ addForm })
+  urlInput.id = 'urlInput'
+  addBtn.id = nameAsString({ addBtn })
+  resetBtn.innerText = 'reset'
+  eraseHistory.innerText = 'Download and Erase History'
+  addBtn.innerText = 'add url'
+  addBtn.addEventListener('click', () => {
+    const regex = /[-a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
+    const url = document.getElementById('urlInput')
+    const { value } = url
+    const isUrl = regex.test(value)
+    if (isUrl) {
+      // add url to list
+      chrome.storage.sync.get('urls', function (result) {
+        const { urls } = result
+        const urlsUpdated = [...urls, value]
+        const ul = document.getElementById('ul')
+        const urlLi = createLi(value, urls)
+        ul.appendChild(urlLi)
+        chrome.storage.sync.set({ urls: urlsUpdated })
+      })
+    } else {
+      document.getElementById('msg').innerHTML = `<div class="error">invalid url</div>`
+    }
+  })
+  addForm.appendChild(urlInput)
+  addForm.appendChild(addBtn)
+  page.appendChild(addForm)
+  page.appendChild(eraseHistory)
+  chrome.storage.sync.get('urls', function (result) {
+    // const urls = result.urls && result.urls.length > 0 ? result.urls : DEFAULT_URLS
+    const { urls } = result
+    const ul = document.createElement('ul')
+    ul.id = nameAsString({ ul })
+    urls.forEach(url => {
+      const urlLi = createLi(url, urls)
+      ul.appendChild(urlLi)
+    })
+    page.appendChild(ul)
+  })
+  page.appendChild(resetBtn)
   document.getElementById('downloader').addEventListener('click', () => {
     downloadExcuses()
   })
