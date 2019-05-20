5f3e4869080db486490e6e6af99c0032bbbcc212
David Kamer <david.kamer@uniwrighte.com>
2019-05-20 13:06:21 -0400

    caught storage error

--- background.js
+++ background.js
@@ -4,8 +4,9 @@ function getTimestamp (date = new Date()) {
 }
 function listener () {
   console.log('triggered')
-  chrome.storage.sync.get(['excuses', 'debounce', 'urls'], function (result) {
+  chrome.storage.sync.get(['excuses', 'debounce', 'urls', 'backupNum'], function (result) {
     const prevExcuses = result.excuses ? result.excuses : [] // TODO - move to storage.onChanged
+    const backupNum = result.backupNum ? result.backupNum : 0
     const { debounce, urls } = result
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
       const { url } = tabs[0]
@@ -13,10 +14,20 @@ function listener () {
       console.log({ isCorrectUrl })
       const debounceUrl = Object.keys(debounce).indexOf(url) !== -1 ? debounce[url] : null
       if ((!debounceUrl || getTimestamp(debounceUrl) + 10 < getTimestamp()) && isCorrectUrl) {
-        const choice = window.prompt('Why did you decide to go to this site?', '')
+        const choice = window.prompt(`Why did you decide to go to ${url}?`)
         const date = new Date().toString()
         const excuses = [ ...prevExcuses, { date, choice, url } ]
-        chrome.storage.sync.set({ excuses, debounce: { ...debounce, [url]: new Date().toString() } })
+        chrome.storage.sync.set({ excuses, debounce: { ...debounce, [url]: new Date().toString() } }, function () {
+          if (chrome.extension.lastError) {
+            const error = chrome.extension.lastError.message
+            console.log({ error })
+            if (error === 'QUOTA_BYTES_PER_ITEM quota exceeded') {
+              const num = backupNum + 1
+              const key = `backup${num}`
+              chrome.storage.sync.set({ excuses: [{ date, choice, url }], [key]: prevExcuses, backupNum: num })
+            }
+          }
+        })
       }
     })
   })
